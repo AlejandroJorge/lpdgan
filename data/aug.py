7ee9ce6e517312f69c4d48adac286e58f706b67b
AlejandroJorge <a20202060@pucp.edu.pe>
2025-02-16 18:58:31 -0500

    aug fix continuation's continuation

--- data/aug.py
+++ data/aug.py
@@ -2,30 +2,32 @@ import albumentations as albu
 
 
 def get_transforms(size):
-    augs = {'weak': albu.Compose([albu.HorizontalFlip(),
-                                  ]),
-            'geometric': albu.OneOf([albu.HorizontalFlip(always_apply=True),
-                                     albu.ShiftScaleRotate(always_apply=True),
-                                     albu.Transpose(always_apply=True),
-                                     albu.OpticalDistortion(always_apply=True),
-                                     albu.ElasticTransform(always_apply=True),
-                                     ])
-            }
+    augs = {'weak': albu.Compose([
+        albu.HorizontalFlip(p=1.0)
+    ]),
+    'geometric': albu.OneOf([
+        albu.HorizontalFlip(p=1.0),
+        albu.ShiftScaleRotate(p=1.0),
+        albu.Transpose(p=1.0),
+        albu.OpticalDistortion(p=1.0),
+        albu.ElasticTransform(p=1.0)
+    ], p=1.0)}
 
     aug_fn = augs['geometric']
     crop_fn = {'random': albu.RandomCrop(height=size[0], width=size[1], p=1.0),
                'center': albu.CenterCrop(height=size[0], width=size[1], p=1.0)}['random']
 
-    effect = albu.OneOf([albu.MotionBlur(blur_limit=21, always_apply=True),
-                         albu.RandomRain(always_apply=True),
-                         albu.RandomFog(always_apply=True),
-                         albu.RandomSnow(always_apply=True)])
-    motion_blur = albu.MotionBlur(blur_limit=55, always_apply=True)
-
+    effect = albu.OneOf([
+        albu.MotionBlur(blur_limit=21, p=1.0),
+        albu.RandomRain(p=1.0),
+        albu.RandomFog(p=1.0),
+        albu.RandomSnow(p=1.0)
+    ], p=1.0)
+    
+    motion_blur = albu.MotionBlur(blur_limit=55, p=1.0)
     resize = albu.Resize(height=size[0], width=size[1])
 
     pipeline = albu.Compose([resize], additional_targets={'target': 'image'})
-
     pipforblur = albu.Compose([effect])
 
     def process(a, b):
