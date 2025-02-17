e428272f4b684cf28f3e661b0fcf82c7b0cee7c4
AlejandroJorge <a20202060@pucp.edu.pe>
2025-02-16 19:10:16 -0500

    aug api changes

--- data/aug.py
+++ data/aug.py
@@ -1,7 +1,7 @@
 import albumentations as albu
 
 
-def get_transforms(size):
+def get_transforms(height, width):
     augs = {'weak': albu.Compose([albu.HorizontalFlip(),
                                   ]),
             'geometric': albu.OneOf([albu.HorizontalFlip(p=1.0),
@@ -13,8 +13,8 @@ def get_transforms(size):
             }
 
     aug_fn = augs['geometric']
-    crop_fn = {'random': albu.RandomCrop(height=size[0], width=size[1], p=1.0),
-               'center': albu.CenterCrop(height=size[0], width=size[1], p=1.0)}['random']
+    crop_fn = {'random': albu.RandomCrop(height=height, width=width, p=1.0),
+               'center': albu.CenterCrop(height=height, width=width, p=1.0)}['random']
 
     effect = albu.OneOf([albu.MotionBlur(blur_limit=21, p=1.0),
                          albu.RandomRain(p=1.0),
@@ -22,7 +22,7 @@ def get_transforms(size):
                          albu.RandomSnow(p=1.0)])
     motion_blur = albu.MotionBlur(blur_limit=55, p=1.0)
 
-    resize = albu.Resize(height=size[0], width=size[1])
+    resize = albu.Resize(height=height, width=width)
 
     pipeline = albu.Compose([resize], additional_targets={'target': 'image'})
 
